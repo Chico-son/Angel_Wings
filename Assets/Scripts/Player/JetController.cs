fa3f579406be0cd8284ba731f16838ed445b5bf2
Santiago H <Thetapaslife@gmail.com>
2026-06-07 02:54:39 -0400

    Add throttle controls
    Basic player movement

--- Assets/Scripts/Player/JetController.cs
+++ Assets/Scripts/Player/JetController.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 public class JetController : MonoBehaviour
 {
+    [Header("Movement Settings")]
     [SerializeField] private float forwardSpeed = 25f;
+    [SerializeField] private float pitchSpeed = 60f;
+    [SerializeField] private float yawSpeed = 45f;
+    [SerializeField] private float rollSpeed = 90f;
+
 
     private Rigidbody rb;
 
@@ -13,8 +18,50 @@ public class JetController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 forwardMovement = transform.forward * forwardSpeed;
-        rb.linearVelocity = forwardMovement;
+        MoveForward();
+        RotateJet();
 
     }
-}   
+
+    private void MoveForward()
+    {
+        Vector3 forwardMovement = transform.forward * forwardSpeed;
+        rb.linearVelocity = forwardMovement;
+    }
+
+    private void RotateJet()
+    {
+       float pitchInput = Input.GetAxis("Vertical");
+       float yawInput = Input.GetAxis("Horizontal");
+
+       float rollInput = 0f;
+
+       if (yawInput.GetKey(KeyCode,Q))
+        {
+            rollInput = 1f;
+        }
+
+        else if (yawInput.GetKey(KeyCode,E))
+        {
+            rollInput = -1f;
+
+        }
+
+        Quaternion pitchRotation = Quaternion.AngleAxis(
+            pitchInput * pitchSpeed * Time.fixedDeltaTime,
+            transform.right
+        );
+
+         Quaternion yawRotation = Quaternion.AngleAxis(
+            yawInput * yawSpeed * Time.fixedDeltaTime,
+            transform.up
+        );
+
+         Quaternion rollRotation = Quaternion.AngleAxis(
+            rollInput * rollSpeed * Time.fixedDeltaTime,
+            transform.forward
+        );
+
+        rb.MoveRotation(rb.rotation * pitchRotation * yawRotation * rollRotation);
+    }   
+}
