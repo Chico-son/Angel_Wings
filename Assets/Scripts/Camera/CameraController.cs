51fda1a6663222502875882541d46b80c6db8c7f
Santiago H <Thetapaslife@gmail.com>
2026-06-07 02:54:10 -0400

    Add third-person camera follow
    Basic player camera

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Follow Target")]
+    [SerializeField] private Transform target;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Camera Settings")]
+    [SerializeField] private Vector3 offset = new Vector3(0f, 3f, -10f);
+    [SerializeField] private float followSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 5f;
+
+    private void LateUpdate()
     {
         
+        if (target == null)
+        {
+            return;
+        }
+        
+        Vector3 desiredPosition = target.position + target.TransformDirection(offset);
+        transform.position = Vector3.Lerp(
+            transform.position,
+            desiredPosition,
+            followSpeed * Time.deltaTime
+        );
+
+        Quaternion desiredRotation = Quaternion.LookRotation(
+            target.position - transform.position,
+            target.up
+        );
+
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            desiredRotation,
+            rotationSpeed * Time.deltaTime
+        );
     }
 }
